798a0cdd0c88dd71c3b57463312b8486d3e4e4d3
Michele Richmond <mmrichmond6@gmail.com>
2023-01-26 16:11:31 -0600

    Add project files.

---
+++ OlderThanMe/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlderThanMe
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name= name;
+            Age= age;
+        }
+
+        public static void CompareAge(Person person1, Person person2)
+        {
+            if (person1.Age > person2.Age) { Console.WriteLine(person2.Name + " is younger than me"); }
+            else if (person1.Age < person2.Age) { Console.WriteLine(person2.Name + " is older than me"); }
+            else if (person1.Age == person2.Age) { Console.WriteLine(person2.Name + " is the same age as me"); }
+            else { Console.WriteLine("Not working"); };
+        }
+    }
+}
---
+++ OlderThanMe/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OlderThanMe;
+
+Person person = new Person("Michele", 30);
+Person p1 = new Person("Samuel", 24);
+Person p2 = new Person("Joel", 36);
+Person p3 = new Person("Lily", 30);
+
+Person.CompareAge(person, p1);
+Person.CompareAge(person, p2);
+Person.CompareAge(person, p3);
+Console.WriteLine("The end");
